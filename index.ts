a18ad7f853140ba50aec66f9c3792f497832b8dc
Talha <83097144+talhaarain666@users.noreply.github.com>
2024-04-04 01:29:28 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let balance = 50000;
+let myPin = 1234;
+
+let answer = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            type: "number",
+            message: "Enter your Pin"
+        }
+    ]
+)
+
+if (answer.pin === myPin) {
+    console.log("Correct Pin Code!")
+
+    let operations = await inquirer.prompt(
+        [
+            {
+                name: "operation",
+                type: "list",
+                message: "Please select option",
+                choices: ["cash withdraw", "check balance"]
+            }
+        ]
+    )
+
+    if (operations.operation === "cash withdraw") {
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    type: "number",
+                    message: "Enter your amount",
+                }
+            ]
+        )
+        balance -= amountAns.amount;
+        console.log(`Your remaining balance is  ${balance}`)
+    } else if (operations.operation === "check balance") {
+        console.log(`Your balance is  ${balance}`)
+
+    }
+
+
+} else {
+    console.log("Incorrect Pin Code!")
+
+}
